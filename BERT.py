07306a0eb57791dde1aaa795448f4d4f7aebe456
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-12-20 23:38:24 +0000

    newBERT

--- BERT.py
+++ BERT.py
@@ -9,11 +9,17 @@ import sklearn
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import *
+import transformers
 from sklearn.metrics import roc_auc_score
 from torch.autograd import Variable
 
-
+print("python version==%s" % python_version())
+print("pandas==%s" % pd.__version__)
+print("numpy==%s" % np.__version__)
+print("torch==%s" % torch.__version__)
+print("sklearn==%s" % sklearn.__version__)
+print("transformers==%s" % transformers.__version__)
+print("matplotlib==%s" % matplotlib.__version__)
 
 df = pd.read_csv('data/train.csv')
 
@@ -33,8 +39,8 @@ pretrained_weights='distilbert-base-uncased'
 # Load pretrained model/tokenizer
 
 # Load pretrained model/tokenizer
-tokenizer = DistilBertTokenizer.from_pretrained(pretrained_weights)
-model = DistilBertModel.from_pretrained(pretrained_weights)
+tokenizer = transformers.DistilBertTokenizer.from_pretrained(pretrained_weights)
+model = transformers.DistilBertModel.from_pretrained(pretrained_weights)
 
 
 
